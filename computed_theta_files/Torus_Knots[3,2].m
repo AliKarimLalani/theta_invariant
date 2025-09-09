a3d0ed42e838efc391622ba3428382891b1281f2
AliKarimLalani <alikarim.lalani@mail.utoronto.ca>
2025-09-09 04:09:15 +0400

    Delete computed_theta_files directory

--- computed_theta_files/Torus_Knots[3,2].m
+++
@@ -1,6 +0,0 @@
-{(1 - T + T^2)/T, (1 - Subscript[T, 1] + Subscript[T, 1]^2 - 
-   Subscript[T, 2] - Subscript[T, 1]^3*Subscript[T, 2] + Subscript[T, 2]^2 + 
-   Subscript[T, 1]^4*Subscript[T, 2]^2 - Subscript[T, 1]*Subscript[T, 2]^3 - 
-   Subscript[T, 1]^4*Subscript[T, 2]^3 + Subscript[T, 1]^2*
-    Subscript[T, 2]^4 - Subscript[T, 1]^3*Subscript[T, 2]^4 + 
-   Subscript[T, 1]^4*Subscript[T, 2]^4)/(Subscript[T, 1]^2*Subscript[T, 2]^2)}
